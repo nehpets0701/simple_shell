118455f7244e044002dd827c935b81367f7c2ca4
nehpets <nehpets0701@gmail.com>
2020-11-12 17:01:34 +0000

    pushing shell header file

---
+++ shellHeader.h
@@ -0,0 +1,15 @@
+#ifndef _shellHeader_H_
+#define _shelHeader_H_
+
+/*headers*/
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/*functions*/
+int main(void);
+#endif
